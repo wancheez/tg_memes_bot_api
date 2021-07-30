320daeee860fbaffa9eb025b568551dbfd55ce91
Ivan Erokhin <wancheez@gmail.com>
2021-07-30 22:17:39 +0300

    static

--- bot_admin_drf/settings.py
+++ bot_admin_drf/settings.py
@@ -125,9 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -139,3 +137,4 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+django_heroku.settings(staticfiles=True)
