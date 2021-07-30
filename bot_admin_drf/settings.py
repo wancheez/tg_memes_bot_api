b8622ae0f6d7a3e50b414ce5b044307055086bcf
Ivan Erokhin <wancheez@gmail.com>
2021-07-30 22:15:30 +0300

    static

--- bot_admin_drf/settings.py
+++ bot_admin_drf/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
