948f678087bb4fdd4aef00834e1b01cb60db2a1f
Ivan Erokhin <wancheez@gmail.com>
2021-07-31 12:36:28 +0300

    repo conf

--- bot_admin_drf/settings.py
+++ bot_admin_drf/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x0n$p2nr3!e*(vee785&yw87qd-ye%eh$wxena!#27*(9=wk&v'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
